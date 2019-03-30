c7cfd9a7e3150ed42bbf72f8885b58168d03a37e
Luther <luther0211@hotmail.com>
2019-03-29 19:09:54 -0600

    install & added clarifai

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Clarifai from 'clarifai';
+
 //COMPONENT IMPORTS
 import InputForm from "./Components/InputForm/InputForm"
 
 
-// API 9a8585cb625d4219b1acedf7c71116f4
+const app = new Clarifai.App({
+    apiKey: '9a8585cb625d4219b1acedf7c71116f4'
+   });
+
 class App extends Component {
 
     onInputChangeHandler = (e) => {
@@ -13,6 +18,13 @@ class App extends Component {
 
     onSubmitHandler = (e) => {
         e.preventDefault()
+        
+        app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg")
+            .then(
+                (res) => {console.log(res)},
+                (err) => {console.log(err)}
+            );
+
         console.log('detecting...')
         
     }
