c2a0fcede2a09f6e0d1acbd1c216e2d21e9231e4
Luther <luther0211@hotmail.com>
2019-03-31 19:25:52 -0600

    added custom Button

--- src/Components/InputForm/InputForm.js
+++ src/Components/InputForm/InputForm.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import {TextField} from "@material-ui/core";
+import { TextField, Button } from "@material-ui/core";
 import "./InputForm.css"
 
 export default props => {
     return (
         <form className="form" onSubmit={props.onInputSubmit} >
             <TextField
-                className="TextField"
+                className="url-TextField"
                 label="Image url"
                 placeholder="Paste an image url!"
                 variant="outlined"
                 InputLabelProps={{ shrink: true}}
             />
-            <div className="input" ><input type="submit" value="Detect" /></div>
+            <Button className="submit-btn" variant="contained" color="primary" type='submit'>Detect</Button>
         </form>
     )
 }
