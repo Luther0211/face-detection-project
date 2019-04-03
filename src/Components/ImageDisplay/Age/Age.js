bf09f9a08f2b3e1b4ecd761df91c8b224cbff28b
Luther <luther0211@hotmail.com>
2019-04-02 22:21:57 -0600

    fixed small bug with switch statement in age component

--- src/Components/ImageDisplay/Age/Age.js
+++ src/Components/ImageDisplay/Age/Age.js
@@ -3,15 +3,15 @@ import React from "react"
 import "./Age.css"
 
 export default ({age, percentage, index}) => {
-    switch(index) {
-        case (index % 2 === 0):
+    switch(true) {
+        case index % 2 === 0:
             return(
                 <div className="data-bar odd">
                     <span className="data">{age} years old</span>
                     <span className="data">{Math.ceil(percentage * 100)}%</span>
                 </div>
             )
-            
+
         default:
             return(
                 <div className="data-bar">
