85ef7aad41946ae931ee414c0e746ce4cf769793
Luther <luther0211@hotmail.com>
2019-04-02 22:41:37 -0600

    created gender component

--- src/Components/ImageDisplay/Age/AgeComponent.js
+++ src/Components/ImageDisplay/Age/AgeComponent.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import "./Age.css"
+import "./AgeComponent.css"
 
 export default ({age, percentage, index}) => {
     switch(true) {
--- src/Components/ImageDisplay/ImageDisplay.js
+++ src/Components/ImageDisplay/ImageDisplay.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import "./ImageDisplay.css"
-import AgeComponent from './Age/Age'
+//COMPONENTS
+import AgeComponent from './Age/AgeComponent'
+import GenderComponent from "./GenderComponent/GenderComponent"
 
 export default ({imageUrl, boxes, onFaceBoxClick, activeFaceData }) => {
+    let age_appearance = null
+    let gender_appearance = null
+    // let multicultural_appearance = null
+     
+    console.log(activeFaceData)
+
     let results = boxes.map(box => {
         let boxSize = {
             top: box.top, 
@@ -13,13 +21,19 @@ export default ({imageUrl, boxes, onFaceBoxClick, activeFaceData }) => {
         return <div className="bounding-box" id={box.id} key={box.id} title={box.id} style={boxSize} onClick={onFaceBoxClick}></div>
     })
 
-    let age = null
 
     if(activeFaceData !== null) {
-        age = activeFaceData.age_appearance.concepts.map((concept, i) => {
-            if(i < 5) return <AgeComponent age={concept.name} percentage={concept.value} key={i} index={i} />
+        //AGE
+        age_appearance = activeFaceData.age_appearance.concepts.map((ageConcept, i) => {
+            if(i < 5) return <AgeComponent age={ageConcept.name} percentage={ageConcept.value} key={i} index={i} />
         })
-        age.unshift(<h2 key="agekey">Age</h2>)
+        age_appearance.unshift(<h2 key="agekey" className="data-title">Age</h2>)
+
+        //GENDER
+        gender_appearance = activeFaceData.gender_appearance.concepts.map((genderConcept, i) => {
+            return <GenderComponent gender={genderConcept.name} percentage={genderConcept.value} key={i} index={i} />
+        })
+        gender_appearance.unshift(<h2 key="genderkey" className="data-title">Gender</h2>)
     }
 
     return (
@@ -34,7 +48,8 @@ export default ({imageUrl, boxes, onFaceBoxClick, activeFaceData }) => {
                 </div>
 
                 <div className="two">
-                    {age}
+                    {age_appearance}
+                    {gender_appearance}
                 </div>
 
             </div>
