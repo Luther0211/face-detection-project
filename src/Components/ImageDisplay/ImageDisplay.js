dfe73e5e805e15ea0be8971f49e35e995d457a00
Luther <luther0211@hotmail.com>
2019-04-03 22:07:13 -0600

    updated css styles

--- src/Components/ImageDisplay/ImageDisplay.js
+++ src/Components/ImageDisplay/ImageDisplay.js
@@ -23,24 +23,25 @@ export default ({imageUrl, boxes, onFaceBoxClick, activeFaceData }) => {
 
     if(activeFaceData !== null) { // COMBINE COMPONENTS!
         //AGE
-        age_appearance = activeFaceData.age_appearance.concepts.map((age, i) => {
-            if(i < 5) return <DataComponent dataName={age.name + " years old"} percentage={age.value} key={i} index={i} />
-        })
-        age_appearance.unshift(<div className="data-instr"> <span>Result</span> <span>Probability %</span> </div>)
+        age_appearance = activeFaceData.age_appearance.concepts.map((age, i) => i < 5 
+            ? <DataComponent dataName={age.name + " years old"} percentage={age.value} key={i} index={i} /> 
+            : null    
+        )
+        age_appearance.unshift(<div className="data-instr" key="data-instr-age"> <span>Result</span> <span>Probability %</span> </div>)
         age_appearance.unshift(<h2 key="agekey" className="data-title">Age</h2>)
 
         //GENDER
         gender_appearance = activeFaceData.gender_appearance.concepts.map((gender, i) => {
             return <DataComponent dataName={gender.name} percentage={gender.value} key={i} index={i} />
         })
-        gender_appearance.unshift(<div className="data-instr"> <span>Result</span> <span>Probability %</span> </div>)
+        gender_appearance.unshift(<div className="data-instr" key="data-instr-gender"> <span>Result</span> <span>Probability %</span> </div>)
         gender_appearance.unshift(<h2 key="genderkey" className="data-title">Gender</h2>)
 
         //multicultural Appearance
         multicultural_appearance = activeFaceData.multicultural_appearance.concepts.map((culturalAp, i) => {
             return <DataComponent dataName={culturalAp.name} percentage={culturalAp.value} index={i} key={i} />
         })
-        multicultural_appearance.unshift(<div className="data-instr"> <span>Result</span> <span>Probability %</span> </div>)
+        multicultural_appearance.unshift(<div className="data-instr" key="data-instr-cultural"> <span>Result</span> <span>Probability %</span> </div>)
         multicultural_appearance.unshift(<h2 key="culturalkey" className="data-title">Multicultural Appearance</h2>)
     }
 
@@ -49,7 +50,7 @@ export default ({imageUrl, boxes, onFaceBoxClick, activeFaceData }) => {
             <div className="main">
 
                 <div className="one">
-                    <div className="absolute">
+                    <div className="image-div">
                         <img id="ImageDisplay" src={imageUrl} alt="" />
                         {results}
                     </div>
