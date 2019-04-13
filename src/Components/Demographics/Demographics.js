18d4e221a13513db022d64bdd6a097f2e065f0c4
Luther <luther0211@hotmail.com>
2019-04-13 18:02:06 -0500

    refactored the Demographics component / cleaned the results component

--- src/Components/Demographics/Demographics.js
+++ src/Components/Demographics/Demographics.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 //Components
 import Form from "../../Elements/Form/Form";
 import Results from "./Results/Results";
+import Data from "./Results/Data/Data"
 
 
 class Demographics extends Component {
@@ -27,6 +28,7 @@ class Demographics extends Component {
                 data = {...output.data.face}
             }
         })
+        
         this.setState({activeOutput: data})
     }
 
@@ -68,6 +70,53 @@ class Demographics extends Component {
             msg = <p className="message">Click on a face!</p>
         }
 
+        //Face boxes
+        let boxResults = this.state.boxes.map(box => {
+            let boxSize = {
+                top: box.top, 
+                right: box.right, 
+                bottom: box.bottom, 
+                left: box.left
+            }
+            return <div className="bounding-box" id={box.id} key={box.id} title={box.id} style={boxSize} onClick={(e) => this.onfaceBoxClickHandler(e)}></div>
+        })
+
+        //Active face data
+        let age_appearance = null
+        let gender_appearance = null
+        let multicultural_appearance = null
+        let resultData = null
+
+        if (this.state.activeOutput !== null) {
+            //AGE
+            age_appearance = this.state.activeOutput.age_appearance.concepts.map((age, i) => i < 5 
+                ? <Data dataName={age.name + " years old"} percentage={age.value} key={`age-${i}`} index={i} /> 
+                : null    
+            )
+            age_appearance.unshift(<div className="data-instr" key="data-instr-age"> <span className="appearance">Age Appearance</span> <span className="probability">Probability %</span> </div>)
+            age_appearance.unshift(<h2 key="agekey" className="data-title">Age</h2>)
+
+            //GENDER
+            gender_appearance = this.state.activeOutput.gender_appearance.concepts.map((gender, i) => {
+                return <Data dataName={gender.name} percentage={gender.value} key={`gender-${i}`} index={i} />
+            })
+            gender_appearance.unshift(<div className="data-instr" key="data-instr-gender"> <span className="appearance">Gender Appearance</span> <span className="probability">Probability %</span> </div>)
+            gender_appearance.unshift(<h2 key="genderkey" className="data-title">Gender</h2>)
+
+            //multicultural Appearance
+            multicultural_appearance = this.state.activeOutput.multicultural_appearance.concepts.map((culturalAp, i) => {
+                return <Data dataName={culturalAp.name} percentage={culturalAp.value} index={i} key={`multicultural-${i}`} />
+            })
+            multicultural_appearance.unshift(<div className="data-instr" key="data-instr-cultural"> <span className="appearance">Multicultural Appearance</span> <span className="probability">Probability %</span> </div>)
+            multicultural_appearance.unshift(<h2 key="culturalkey" className="data-title">Multicultural Appearance</h2>)
+            
+            //Final
+            console.log(this.state.activeOutput)
+            resultData = [...age_appearance, ...gender_appearance, ...multicultural_appearance]
+        }
+
+
+
         return (
             <div className="App"> 
                 
@@ -80,9 +129,8 @@ class Demographics extends Component {
                 
                 <Results 
                     imageUrl={this.state.imageURL} 
-                    boxes={this.state.boxes} 
-                    onFaceBoxClick={(e) => this.onfaceBoxClickHandler(e)} 
-                    activeFaceData={this.state.activeOutput} 
+                    boxes={boxResults}
+                    resultData={resultData} 
                 />
                 
             </div>
--- src/Components/Demographics/Results/Results.js
+++ src/Components/Demographics/Results/Results.js
@@ -1,64 +1,22 @@
 import React from 'react'
 import "./Results.css"
 //COMPONENTS
-import Data from "./Data/Data"
 
-export default ({imageUrl, boxes, onFaceBoxClick, activeFaceData }) => {
-    let age_appearance = null
-    let gender_appearance = null
-    let multicultural_appearance = null
-     
-    let boxResults = boxes.map(box => {
-        let boxSize = {
-            top: box.top, 
-            right: box.right, 
-            bottom: box.bottom, 
-            left: box.left
-        }
-        return <div className="bounding-box" id={box.id} key={box.id} title={box.id} style={boxSize} onClick={onFaceBoxClick}></div>
-    })
-
-
-    if(activeFaceData !== null) { // COMBINE COMPONENTS!
-        //AGE
-        age_appearance = activeFaceData.age_appearance.concepts.map((age, i) => i < 5 
-            ? <Data dataName={age.name + " years old"} percentage={age.value} key={i} index={i} /> 
-            : null    
-        )
-        age_appearance.unshift(<div className="data-instr" key="data-instr-age"> <span className="appearance">Age Appearance</span> <span className="probability">Probability %</span> </div>)
-        age_appearance.unshift(<h2 key="agekey" className="data-title">Age</h2>)
-
-        //GENDER
-        gender_appearance = activeFaceData.gender_appearance.concepts.map((gender, i) => {
-            return <Data dataName={gender.name} percentage={gender.value} key={i} index={i} />
-        })
-        gender_appearance.unshift(<div className="data-instr" key="data-instr-gender"> <span className="appearance">Gender Appearance</span> <span className="probability">Probability %</span> </div>)
-        gender_appearance.unshift(<h2 key="genderkey" className="data-title">Gender</h2>)
-
-        //multicultural Appearance
-        multicultural_appearance = activeFaceData.multicultural_appearance.concepts.map((culturalAp, i) => {
-            return <Data dataName={culturalAp.name} percentage={culturalAp.value} index={i} key={i} />
-        })
-        multicultural_appearance.unshift(<div className="data-instr" key="data-instr-cultural"> <span className="appearance">Multicultural Appearance</span> <span className="probability">Probability %</span> </div>)
-        multicultural_appearance.unshift(<h2 key="culturalkey" className="data-title">Multicultural Appearance</h2>)
-    }
-
+export default (props) => {
     return (
         <div className="results">
             
             <div className="result result-image">
 
                 <div className="image-div">
-                    <img id="DemographicsResults" src={imageUrl} alt="" />
-                    {boxResults}
+                    <img id="DemographicsResults" src={props.imageUrl} alt="" />
+                    {props.boxes}
                 </div>
                
             </div>
 
             <div className="result result-data">
-                {age_appearance}
-                {gender_appearance}
-                {multicultural_appearance}
+                {props.resultData}
             </div>
 
         </div>
