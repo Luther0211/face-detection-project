6a8325e0959cba5be2660011cb698f6aed8bcf07
Luther <luther0211@hotmail.com>
2019-05-16 22:20:56 -0500

    Updated HOME button in Form component

--- src/Elements/Form/Form.js
+++ src/Elements/Form/Form.js
@@ -10,7 +10,7 @@ import "./Form.css"
 export default props => {
     return (
         <div className="top-section">
-            <Button className="submit-btn" variant="contained" color="primary" type='submit'>
+            <Button className="submit-btn HOME" variant="contained" color="primary" type='submit'>
                 <Link to="/" className="Link">HOME</Link>
             </Button>
             <h1 className="top-title">{props.title}</h1>
